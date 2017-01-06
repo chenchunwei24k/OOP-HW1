1b2e822d754b9d081f26062a04d9b96cad3bcd76
chenchunwei24k@gmail.com <chenchunwei24k@gmail.com>
2017-01-06 20:02:05 +0800

    HW1 補教

---
+++ src/fcu/iecs/oop/Hello.java
@@ -0,0 +1,14 @@
+package fcu.iecs.oop;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		String ly="Let it go!Cannot hold it back anymore";
+		String ly2=ly.replace("it", "her");
+		String ly3=ly2.replace("Cannot","Can't");
+		System.out.println(ly3);
+
+	}
+
+}
